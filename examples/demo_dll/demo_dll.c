96bb36f3e9465fb65c6fce884afbbaaabf81dbdf
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-04-27 18:30:50 +0200

    dll cleanup

--- examples/demo_dll/demo_dll.c
+++ examples/demo_dll/demo_dll.c
@@ -1,77 +1,17 @@
 #include "demo_dll.h"
 
 void __stdcall DEMOROUTINE_DUMP demo_routine(
-	char *ch_str1,
-	int ch_lenstr1,
-	char *ch_str2, 
-	int ch_lenstr2,
-	struct L1 *Log1,
-	struct C1 *Str1,
-	struct I1 *Int1,
-	struct R1 *Real1,
-	struct ResC *Resultc,
-	struct ResV *Resultv
+	char *param_char_p,
+	int param_int,
+	struct test *param_struct_test_p
 	)
 {
 	
-	FILE *fd_log1_a;
-	fd_log1_a = fopen("dump_log1.txt","w");
-	fprintf(fd_log1_a, "Depart = '%d' \n", (*Log1).Depart);
-	fprintf(fd_log1_a, "Flyby = '%d' \n", (*Log1).Flyby);
-	fprintf(fd_log1_a, "Jetis = '%d' \n", (*Log1).Jetis);
-	fprintf(fd_log1_a, "Jetisd = '%d' \n", (*Log1).Jetisd);
-	fprintf(fd_log1_a, "Keep = '%d' \n", (*Log1).Keep);
-	fprintf(fd_log1_a, "Printanom = '%d' \n", (*Log1).Printanom);
-	fprintf(fd_log1_a, "Retro = '%d' \n", (*Log1).Retro);
-	fprintf(fd_log1_a, "Write_output = '%d' \n", (*Log1).Write_output);
-	fprintf(fd_log1_a, "Zero = '%d' \n", (*Log1).Zero);
-	fprintf(fd_log1_a, "Dump[0] = '%d' \n", (*Log1).Dump[0]);
-	fprintf(fd_log1_a, "Dump[1] = '%d' \n", (*Log1).Dump[1]);
-	fprintf(fd_log1_a, "Dump[2] = '%d' \n", (*Log1).Dump[2]);
-	fprintf(fd_log1_a, "Dump[3] = '%d' \n", (*Log1).Dump[3]);
-	fclose(fd_log1_a);
-	
-	FILE *fd_int1_a;
-	fd_int1_a = fopen("dump_int1.txt","w");
-	fprintf(fd_int1_a, "Choice = '%d' \n", (*Int1).Choice);
-	fprintf(fd_int1_a, "Ia = '%d' \n", (*Int1).Ia);
-	fprintf(fd_int1_a, "Icb = '%d' \n", (*Int1).Icb);
-	fprintf(fd_int1_a, "Il = '%d' \n", (*Int1).Il);
-	fprintf(fd_int1_a, "Iname = '%d' \n", (*Int1).Iname);
-	fprintf(fd_int1_a, "Iprnt = '%d' \n", (*Int1).Iprnt);
-	fprintf(fd_int1_a, "Itmax = '%d' \n", (*Int1).Itmax);
-	fprintf(fd_int1_a, "Mjet = '%d' \n", (*Int1).Mjet);
-	fprintf(fd_int1_a, "Nb1 = '%d' \n", (*Int1).Nb1);
-	fprintf(fd_int1_a, "Nb1 = '%d' \n", (*Int1).Nb2);
-	fprintf(fd_int1_a, "Ncop = '%d' \n", (*Int1).Ncop);
-	fprintf(fd_int1_a, "Nctopy = '%d' \n", (*Int1).Nctopy);
-	fprintf(fd_int1_a, "Nlv = '%d' \n", (*Int1).Nlv);
-	fprintf(fd_int1_a, "Nmuop = '%d' \n", (*Int1).Nmuop);
-	fprintf(fd_int1_a, "Nout = '%d' \n", (*Int1).Nout);
-	fprintf(fd_int1_a, "Npow = '%d' \n", (*Int1).Npow);
-	fprintf(fd_int1_a, "Nsr = '%d' \n", (*Int1).Nsr);
-	fprintf(fd_int1_a, "Nt = '%d' \n", (*Int1).Nt);
-	fprintf(fd_int1_a, "Nv1 = '%d' \n", (*Int1).Nv1);
-	fprintf(fd_int1_a, "Nv2 = '%d' \n", (*Int1).Nv2);
-	fprintf(fd_int1_a, "Eopt[0] = '%d' \n", (*Int1).Eopt[0]);
-	fprintf(fd_int1_a, "Eopt[1] = '%d' \n", (*Int1).Eopt[1]);
-	fprintf(fd_int1_a, "Ic[0] = '%d' \n", (*Int1).Ic[0]);
-	fprintf(fd_int1_a, "Ic[1] = '%d' \n", (*Int1).Ic[1]);
-	fprintf(fd_int1_a, "Ip[0] = '%d' \n", (*Int1).Ip[0]);
-	fprintf(fd_int1_a, "Ip[1] = '%d' \n", (*Int1).Ip[1]);
-	fprintf(fd_int1_a, "Iu[0] = '%d' \n", (*Int1).Iu[0]);
-	fprintf(fd_int1_a, "Iu[1] = '%d' \n", (*Int1).Iu[1]);
-	fprintf(fd_int1_a, "Jdl[0] = '%d' \n", (*Int1).Jdl[0]);
-	fprintf(fd_int1_a, "Jdl[1] = '%d' \n", (*Int1).Jdl[1]);
-	fprintf(fd_int1_a, "Jdl[2] = '%d' \n", (*Int1).Jdl[2]);
-	fprintf(fd_int1_a, "Jdl[3] = '%d' \n", (*Int1).Jdl[3]);
-	fprintf(fd_int1_a, "Jdl[4] = '%d' \n", (*Int1).Jdl[4]);
-	fprintf(fd_int1_a, "Jdl[5] = '%d' \n", (*Int1).Jdl[5]);
-	fprintf(fd_int1_a, "Nflag[0] = '%d' \n", (*Int1).Nflag[0]);
-	fprintf(fd_int1_a, "Nflag[1] = '%d' \n", (*Int1).Nflag[1]);
-	fclose(fd_int1_a);
-	
-        printf("Dump!\n");
+	printf("el_int8t = '%d' \n", (*param_struct_test_p).el_int8t);
+	printf("el_int8t_4[0] = '%d' \n", (*param_struct_test_p).el_int8t_4[0]);
+	printf("el_int8t_4[1] = '%d' \n", (*param_struct_test_p).el_int8t_4[1]);
+	printf("el_int8t_4[2] = '%d' \n", (*param_struct_test_p).el_int8t_4[2]);
+	printf("el_int8t_4[3] = '%d' \n", (*param_struct_test_p).el_int8t_4[3]);
 	
 }
 
--- examples/demo_dll/demo_dll.h
+++ examples/demo_dll/demo_dll.h
@@ -15,165 +15,21 @@ typedef int32_t bool;
 #define TRUE  1
 #define FALSE 0
 
-struct L1
+struct test
 {
-	int8_t Depart;
-	int8_t Flyby;
-	int8_t Jetis;
-	int8_t Jetisd;
-	int8_t Keep;
-	int8_t Printanom;
-	int8_t Retro;
-	int8_t Write_output;
-	int8_t Zero;
-	int8_t Dump[4]; // (3)
-};
-
-struct C1
-{
-	char Acpath[48]; // As String * 48
-	char Sarray[8]; // As String * 8
-	char Bulsi[8]; //  As String * 8
-	char Head[120]; // As String * 120
-	char Ptype[1]; //  As String * 1
-	char Acname[4][12]; // (3) As String * 12
-	char Shota[4][8]; // (3) As String * 8
-};
-
-struct I1
-{
-	int16_t Choice;
-	int16_t Ia;
-	int16_t Icb;
-	int16_t Il;
-	int16_t Iname;
-	int16_t Iprnt;
-	int16_t Itmax;
-	int16_t Mjet;
-	int16_t Nb1;
-	int16_t Nb2;
-	int16_t Ncop;
-	int16_t Nctopy;
-	int16_t Nlv;
-	int16_t Nmuop;
-	int16_t Nout;
-	int16_t Npow;
-	int16_t Nsr;
-	int16_t Nt;
-	int16_t Nv1;
-	int16_t Nv2;
-	int16_t Eopt[2]; // (1)
-	int16_t Ic[2]; // (1)
-	int16_t Ip[2]; // (1)
-	int16_t Iu[2]; // (1)
-	int16_t Jdl[6]; // (5)
-	int16_t Nflag[2]; // (1)
-};
-
-struct R1
-{
-
-	double Accelflg;
-	double Acf;
-	double Alt;
-	double Alta;
-	double Altv;
-	double Bb;
-	double Cisp;
-	double Contin;
-	double Dd;
-	double Delpo;
-	double Epoch;
-	double Fxdmassd;
-	double Ispd;
-	double Glossd;
-	double Kadp;
-	double Ks;
-	double Kt;
-	double Ktd;
-	double Lvcont;
-	double M0;
-	double M0d;
-	double Madp;
-	double Pmin;
-	double Ptilt;
-	double Ra;
-	double Radep;
-	double Rarr;
-	double Rarv;
-	double Rdep;
-	double Rn;
-	double Rp;
-	double Rparr;
-	double Rpdep;
-	double Rpe;
-	double Rtilt;
-	double Sap;
-	double T0;
-	double Tstay;
-	double Alfa[2]; // (1) 
-	double Gmp[2]; // (1) 
-	double Kd[2]; // (1) 
-	double Kr[2]; // (1) 
-	double Rpl[2]; // (1) 
-	double P0[3]; // (2) 
-	double Adate[3]; // (2) 
-	double Is_r[3]; // (2) 
-	double Isp[3]; // (2) 
-	double Jdate[3]; // (2) 
-	double Tend[3]; // (2) 
-	double V1[3]; // (2) 
-	double V2[3]; // (2) 
-	double Vhl[3]; // (2) 
-	double Vhp[3]; // (2) 
-	double Mlv[4]; // (3) 
-	double Gamap[5]; // (4) 
-	double Pcons[5]; // (4) 
-	double Orb1[6]; // (5) 
-	double Orb2[6]; // (5) 
-	double Xa[12]; // (11) 
-	double Xl[12]; // (11) 
-};
-
-struct ResC
-{
-	char Bca[12]; // As String * 12
-	char Bcl[12]; // As String * 12
-	char Bodies[2][30]; // (1 To 2) As String * 30
-	char Bcd[37][6]; // (1 To 37) As String * 6
-	char str0n[30][4]; // (1 To 30) As String * 4
-	char strLine[3][132]; // (1 To 3) As String * 132
-	char strUn[2][2]; // (1 To 2) As String * 2
-};
-
-struct ResV
-{
-	double Date[2]; // (1 To 2);
-	double Vel1;
-	double Vel2;
-	double Ps;
-	double Data[11][35]; // (1 To 11, 1 To 35)
-	double Pr[37]; // (36)
-	double OnValues[30]; // (1 To 30)
-	double Time[2]; // (1 To 2)
-	double M0[2]; // (1 To 2);
-	double M[2]; // (1 To 2); HACK: was "M"
-	double Mp[2]; // (1 To 2);
-	int16_t Max;
-	int16_t NoOn;
+	char el_char;
+	int8_t el_int8t;
+	int16_t el_int16t;
+	float el_float;
+	double el_double;
+	int8_t el_int8t_4[4];
+	int8_t el_int8t_2x3[2][3];
 };
 
 void __stdcall DEMOROUTINE_DUMP demo_routine(
-	char *ch_str1,
-	int ch_lenstr1,
-	char *ch_str2, 
-	int ch_lenstr2,
-	struct L1 *Log1,
-	struct C1 *Str1,
-	struct I1 *Int1,
-	struct R1 *Real1,
-	struct ResC *Resultc,
-	struct ResV *Resultv
+	char *param_char_p,
+	int param_int,
+	struct test *param_struct_test_p
 	);
 
 DEMOROUTINE_DUMP bool __stdcall DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved);
