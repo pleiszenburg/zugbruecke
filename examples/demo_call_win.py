95c6343d6d5913792fed435b9b28c091fcc5b361
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-05-06 19:05:30 +0200

    demo call works under wine

--- examples/demo_call_win.py
+++ examples/demo_call_win.py
@@ -41,7 +41,7 @@ class demo_routine_caller_class():
 		
 		# Define parameter datatypes
 		self._call_demo_routine_.argtypes = [
-			ctypes.c_char_p,
+			ctypes.POINTER(ctypes.c_char),
 			ctypes.c_int,
 			ctypes.POINTER(type_test_struct)
 			]
@@ -50,22 +50,17 @@ class demo_routine_caller_class():
 		self._call_demo_routine_.restype = ctypes.c_void_p
 	
 	
-	def call(self, param_char_p, param_int, param_type_test):
+	def call(self, param_char, param_int, param_type_test):
 		
-		try:
+		# Call routine in DLL
+		self._call_demo_routine_(
+			ctypes.byref(param_char), 
+			param_int, 
+			ctypes.byref(param_type_test)
+			)
 		
-			# Call routine in DLL
-			self._call_demo_routine_(
-				param_char_p, 
-				param_int, 
-				ctypes.byref(param_type_test)
-				)
-			
-			return True
-			
-		except:
-			
-			return False
+		print('Called!')
+		return True
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -74,23 +69,23 @@ class demo_routine_caller_class():
 
 if __name__ == '__main__':
 	
-	# Initialize caller
-	demo_routine_caller = demo_routine_caller_class()
-	
 	# Initialize some test parameters
-	sample_param_char_p = 7
-	sample_param_int = 42
+	sample_param_char = ctypes.c_char(7)
+	sample_param_int = ctypes.c_int(42)
 	sample_type_test = type_test_struct()
-	sample_type_test.el_char = 5
-	sample_type_test.el_int8t = 17
-	sample_type_test.el_int16t = 25874
-	sample_type_test.el_float = 2.4
-	sample_type_test.el_double = 8.598e31
+	sample_type_test.el_char = ctypes.c_char(5)
+	sample_type_test.el_int8t = ctypes.c_int8(17)
+	sample_type_test.el_int16t = ctypes.c_int16(25874)
+	sample_type_test.el_float = ctypes.c_float(2.4)
+	sample_type_test.el_double = ctypes.c_double(8.598e31)
 	el_int8t_4 = [1, 2, 3, 4]
 	sample_type_test.el_int8t_4 = (ctypes.c_int8 * len(el_int8t_4))(*el_int8t_4)
 	el_int8t_2x3 = [[1, 2], [3, 4], [5, 6]]
 	sample_type_test.el_int8t_2x3 = (ctypes.c_int8 * len(el_int8t_2x3[0]) * len(el_int8t_2x3))(*(tuple(i) for i in el_int8t_2x3))
 	
+	# Initialize caller
+	demo_routine_caller = demo_routine_caller_class()
+	
 	# Call
-	demo_routine_caller.call(sample_param_char_p, sample_param_int, sample_type_test)
+	demo_routine_caller.call(sample_param_char, sample_param_int, sample_type_test)
 
