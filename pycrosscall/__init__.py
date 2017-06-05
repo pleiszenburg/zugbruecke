1465cf85c2da64ae26ba86817ea53589d19ab3ec
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-05 12:10:36 +0200

    added unique session id

--- pycrosscall/__init__.py
+++ pycrosscall/__init__.py
@@ -1,11 +1,24 @@
 
 import atexit as _atexit
+import random as _random
+
+
+def _get_randhashstr(dig):
+
+	return (('%0' + str(dig) + 'x') % _random.randrange(16**dig))
+
+
+def _generate_session_id():
+
+	return _get_randhashstr(8)
+
 
 def _unload_module():
 
-	print('LINUX: pycrosscall unloaded')
+	print('LINUX (%s): pycrosscall unloaded' % session_id)
 
 
-print('LINUX: pycrosscall imported')
+session_id = _generate_session_id()
+print('LINUX (%s): pycrosscall imported' % session_id)
 
 _atexit.register(_unload_module)
