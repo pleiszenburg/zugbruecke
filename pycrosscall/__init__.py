753e02a560abacd891f1c9afd119f59969195fee
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-05 12:04:04 +0200

    module loads and unloads

--- pycrosscall/__init__.py
+++ pycrosscall/__init__.py
@@ -1,2 +1,11 @@
 
-print('pycrosscall imported')
+import atexit as _atexit
+
+def _unload_module():
+
+	print('LINUX: pycrosscall unloaded')
+
+
+print('LINUX: pycrosscall imported')
+
+_atexit.register(_unload_module)
