76593a38f4bc70929a694ba85cc5e707d310b346
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-05 12:27:01 +0200

    wine interface becomes a class

--- pycrosscall/__init__.py
+++ pycrosscall/__init__.py
@@ -3,7 +3,7 @@ import atexit as _atexit
 import random as _random
 import os as _os
 
-from .wine import _translate_path_unix2win
+from .wine import wine_session_class as _wine_session_class
 
 
 def _get_randhashstr(dig):
@@ -24,8 +24,9 @@ def _unload_module():
 session_id = _generate_session_id()
 print('UNIX (%s): pycrosscall imported' % session_id)
 
+wine_session = _wine_session_class(session_id)
 test_path_unix = _os.path.split(_os.path.realpath(__file__))[0]
-test_path_win, err = _translate_path_unix2win(test_path_unix)
+test_path_win, err = wine_session._translate_path_unix2win(test_path_unix)
 print(test_path_unix)
 print(test_path_win)
 print(len(err), err)
--- pycrosscall/wine.py
+++ pycrosscall/wine.py
@@ -9,17 +9,24 @@ import subprocess
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# IMPORT
+# WINE SESSION CLASS
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def _translate_path_unix2win(path):
+class wine_session_class:
 
-	winepath_p = subprocess.Popen(
-		['winepath', '-w', path],
-		stdin = subprocess.PIPE,
-		stdout = subprocess.PIPE,
-		stderr = subprocess.PIPE
-		)
-	wine_out, wine_err = winepath_p.communicate()
+	def __init__(self, session_id):
 
-	return wine_out.decode(encoding = 'UTF-8').strip(), wine_err.decode(encoding = 'UTF-8')
+		self.id = session_id
+
+
+	def _translate_path_unix2win(self, path):
+
+		winepath_p = subprocess.Popen(
+			['winepath', '-w', path],
+			stdin = subprocess.PIPE,
+			stdout = subprocess.PIPE,
+			stderr = subprocess.PIPE
+			)
+		wine_out, wine_err = winepath_p.communicate()
+
+		return wine_out.decode(encoding = 'UTF-8').strip(), wine_err.decode(encoding = 'UTF-8')
