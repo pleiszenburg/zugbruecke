c23986cced35c0336fd940ad5e48797a22bc12cc
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-08 16:59:23 +0200

    prepared next steps, argument and return value types

--- examples/test_pycrosscall.py
+++ examples/test_pycrosscall.py
@@ -7,9 +7,24 @@ pycrosscall_path = os.path.abspath(os.path.join(os.path.split(os.path.realpath(_
 sys.path.insert(0, pycrosscall_path)
 
 from pycrosscall import ctypes
+
 _call_demo_routine_ = ctypes.windll.LoadLibrary('demo_dll.dll').simple_demo_routine
-print('Loaded!')
-_call_demo_routine_(20)
+print('DLL and routine loaded!')
+
+_call_demo_routine_.argtypes = [
+	ctypes.c_float,
+	ctypes.c_float
+	]
+print('Set argument types!')
+
+_call_demo_routine_.restype = ctypes.c_float
+print('Set return value type!')
+
+return_value = _call_demo_routine_(20.0, 1.07)
 print('Called!')
+try:
+	print('Got "%f".' % return_value)
+except:
+	pass
 
 time.sleep(8)
