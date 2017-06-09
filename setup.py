12052155d21d7e6fd49b72b20fd7d98d4dd2a098
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-09 12:13:23 +0200

    added setup.py and setup.cfg for pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from distutils.core import setup
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# SETUP
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+setup(
+	name = 'pycrosscall',
+	packages = ['pycrosscall'],
+	version = '0.0.1',
+	description = ' Calling routines in Windows DLLs from Python scripts running under Linux',
+	author = 'Sebastian M. Ernst',
+	author_email = 'ernst@pleiszenburg.de',
+	url = 'https://github.com/s-m-e/pycrosscall',
+	download_url = 'https://github.com/s-m-e/pycrosscall/archive/pycrosscall_0.0.1.tar.gz',
+	keywords = ['ctypes', 'wine'],
+	classifiers = []
+)
