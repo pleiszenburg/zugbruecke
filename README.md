cb003e5c675668da079afcd52bff6a4aff2e1938
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-09 12:34:04 +0200

    changed description

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # pycrosscall
-Calling routines in Windows DLLs from Python scripts running under Linux (and possibly vice versa)
+Calling routines in Windows DLLs from Python scripts running under Linux, MacOS or BSD
 
 Requires Wine 2.x and CPython 3.5.x for Windows.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
 	name = 'pycrosscall',
 	packages = ['pycrosscall'],
 	version = '0.0.1',
-	description = ' Calling routines in Windows DLLs from Python scripts running under Linux',
+	description = 'Calling routines in Windows DLLs from Python scripts running under Linux, MacOS or BSD',
 	author = 'Sebastian M. Ernst',
 	author_email = 'ernst@pleiszenburg.de',
 	url = 'https://github.com/s-m-e/pycrosscall',
