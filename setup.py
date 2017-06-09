0ee50cb0e6b156f4e9fdeba0786696dfa0655e8c
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-09 12:51:55 +0200

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,29 @@ setup(
 	packages = ['pycrosscall'],
 	version = '0.0.1',
 	description = 'Calling routines in Windows DLLs from Python scripts running under Linux, MacOS or BSD',
+	long_description = 'Calling routines in Windows DLLs from Python scripts running under Linux, MacOS or BSD', # TODO improve
 	author = 'Sebastian M. Ernst',
 	author_email = 'ernst@pleiszenburg.de',
 	url = 'https://github.com/s-m-e/pycrosscall',
 	download_url = 'https://github.com/s-m-e/pycrosscall/archive/pycrosscall_0.0.1.tar.gz',
 	keywords = ['ctypes', 'wine'],
-	classifiers = []
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+		'Operating System :: MacOS',
+		'Operating System :: POSIX :: BSD',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3 :: Only',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Topic :: Scientific/Engineering',
+		'Topic :: Software Development',
+		'Topic :: System :: Operating System',
+		'Topic :: System :: Operating System Kernels',
+		'Topic :: Utilities'
+		]
 )
