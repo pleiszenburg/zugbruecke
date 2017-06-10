7bbe62b529fac1a43aea2e8ceba9659adeee638f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-10 13:02:23 +0200

    performance related code

--- examples/test_pycrosscall_perf.py
+++ examples/test_pycrosscall_perf.py
@@ -2,29 +2,21 @@
 import sys
 import os
 import time
+import timeit
+from sys import platform
 
 pycrosscall_path = os.path.abspath(os.path.join(os.path.split(os.path.realpath(__file__))[0], '..'))
 sys.path.insert(0, pycrosscall_path)
 
 from pycrosscall import ctypes
+if True in [platform.startswith(os_name) for os_name in ['linux', 'darwin', 'freebsd']]:
+	ctypes.windll.start_session(parameter = {'log_level': 0})
 
 _call_demo_routine_ = ctypes.windll.LoadLibrary('demo_dll.dll').simple_demo_routine
-print('DLL and routine loaded!')
-
 _call_demo_routine_.argtypes = [
 	ctypes.c_float,
 	ctypes.c_float
 	]
-print('Set argument types!')
-
 _call_demo_routine_.restype = ctypes.c_float
-print('Set return value type!')
 
-return_value = _call_demo_routine_(20.0, 1.07)
-print('Called!')
-try:
-	print('Got "%f".' % return_value)
-except:
-	print('Got no return value!')
-
-time.sleep(8)
+timeit.timeit('return_value = _call_demo_routine_(20.0, 1.07)', number = 10)
