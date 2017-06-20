d260d9b1c012d3421e52dcd9c7b5c0971af93c7e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-06-20 17:32:03 +0200

    fixed comments

--- pycrosscall/__init__.py
+++ pycrosscall/__init__.py
@@ -29,7 +29,7 @@ specific language governing rights and limitations under the License.
 # Load windll_class for mimicing ctypes.windll eventually, make it private
 from .core import windll_class as __windll_class__
 
-# Set up and expose windll, starting session while doing so
+# Set up and expose windll, prepare (but do not start) session while doing so
 windll = __windll_class__()
 
 # Import and expose ctypes
