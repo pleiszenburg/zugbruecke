57ceddce96fcab483cdb8af69ef209e7a105138b
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-02-03 16:03:53 +0100

    converted tests to new import pattern

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -35,7 +35,7 @@ import pytest
 
 from sys import platform
 if any([platform.startswith(os_name) for os_name in ['linux', 'darwin', 'freebsd']]):
-	import zugbruecke as ctypes
+	import zugbruecke.ctypes as ctypes
 elif platform.startswith('win'):
 	import ctypes
 
