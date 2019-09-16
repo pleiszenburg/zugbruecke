eca27b5c2b68d5cf0190540fab7164f86beb24e8
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-09-16 13:57:32 +0200

    extended poc

--- src/zugbruecke/wenv/__main__.py
+++ src/zugbruecke/wenv/__main__.py
@@ -5,8 +5,10 @@ from .lib import env_class
 
 env = env_class()
 
+import sys; print(sys.argv)
+
 os.execvpe(
 	'wine',
-	('wine', env._path_dict_['interpreter'],),
+	('wine', env._path_dict_['interpreter'], *sys.argv[1:]),
 	env._envvar_dict_,
 	)
