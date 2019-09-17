cfa40a0aa3592df7e0064ff87d7176fed4885fe9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-09-17 12:11:47 +0200

    debugging travis 2

--- src/zugbruecke/wenv.py
+++ src/zugbruecke/wenv.py
@@ -202,6 +202,13 @@ class env_class:
 			).communicate()
 		print(out.decode('utf-8'))
 		print(err.decode('utf-8'))
+		out, err = subprocess.Popen(
+			['which', 'wine'],
+			stdout = subprocess.PIPE, stderr = subprocess.PIPE,
+			env = self._envvar_dict
+			).communicate()
+		print(out.decode('utf-8'))
+		print(err.decode('utf-8'))
 		proc = subprocess.Popen(
 			['wine', 'wineboot', '-i'],
 			stdout = subprocess.PIPE, stderr = subprocess.PIPE,
