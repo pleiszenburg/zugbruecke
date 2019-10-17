ab2868f7e231c8dd125d7b3d05f92d1c34bbb178
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-10-17 21:48:14 +0200

    central ctypes/zugbruecke import switch for tests

---
+++ tests/lib/ctypes.py
@@ -0,0 +1,9 @@
+
+from sys import platform
+
+if any([platform.startswith(os_name) for os_name in ['linux', 'darwin', 'freebsd']]):
+	import zugbruecke.ctypes as ctypes
+elif platform.startswith('win'):
+	import ctypes
+else:
+	raise SystemError('unsopported platform')
