f3567d30e431cb101c8b7d101718424aceb453a9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-11-02 13:51:20 +0100

    prevent silent test failures due to architecture mismatches

--- tests/lib/ctypes.py
+++ tests/lib/ctypes.py
@@ -1,16 +1,21 @@
 
 import os
 from sys import platform
+from platform import architecture
 
 from .const import ARCHS, CONVENTIONS
 from .names import get_dll_path
 
+ARCHITECTURE = architecture()[0][:2]
+
 if any([platform.startswith(os_name) for os_name in ['linux', 'darwin', 'freebsd']]):
 	import zugbruecke
 	CTYPES = {arch: zugbruecke.ctypes_session({'arch': arch}) for arch in ARCHS}
+	PLATFORM = 'unix'
 elif platform.startswith('win'):
 	import ctypes
 	CTYPES = {arch: ctypes for arch in ARCHS}
+	PLATFORM = 'wine'
 else:
 	raise SystemError('unsopported platform')
 
@@ -28,7 +33,5 @@ def get_dll_handles(test_path):
 
 	for convention in CONVENTIONS:
 		for arch in ARCHS:
-			try:
+			if PLATFORM == 'unix' or arch[3:] == ARCHITECTURE:
 				yield (CTYPES[arch], get_dll_handle(arch, convention, test_fn))
-			except OSError:
-				print('Loading DLL for testfile "%s" on arch "%s" failed.' % (test_fn, arch))
