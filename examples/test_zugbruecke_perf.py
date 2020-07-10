84d207da7a44c83c5a5edb2c23194bca41dff5f8
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-10 18:06:02 +0200

    update copyright

--- examples/test_zugbruecke_perf.py
+++ examples/test_zugbruecke_perf.py
@@ -11,7 +11,7 @@ https://github.com/pleiszenburg/zugbruecke
 
 	Required to run on platform / side: [UNIX, WINE]
 
-	Copyright (C) 2017-2019 Sebastian M. Ernst <ernst@pleiszenburg.de>
+	Copyright (C) 2017-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
 <LICENSE_BLOCK>
 The contents of this file are subject to the GNU Lesser General Public License
