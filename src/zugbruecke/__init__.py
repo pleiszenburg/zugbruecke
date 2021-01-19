6a297a67d0e1faf0f7927fe53aab385d3edd8534
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-01-19 13:53:33 +0100

    cleanup

--- src/zugbruecke/__init__.py
+++ src/zugbruecke/__init__.py
@@ -6,11 +6,11 @@ ZUGBRUECKE
 Calling routines in Windows DLLs from Python scripts running on unixlike systems
 https://github.com/pleiszenburg/zugbruecke
 
-	src/zugbruecke/__init__.py: Package init file
+    src/zugbruecke/__init__.py: Package init file
 
-	Required to run on platform / side: [UNIX, WINE]
+    Required to run on platform / side: [UNIX, WINE]
 
-	Copyright (C) 2017-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+    Copyright (C) 2017-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
 <LICENSE_BLOCK>
 The contents of this file are subject to the GNU Lesser General Public License
