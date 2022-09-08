b29d4c128b7366e9320e887ef3e7a0b4e51a753f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-09-08 14:15:33 +0200

    move tests from setup.cfg to pyproject

--- tests/lib/names.py
+++ tests/lib/names.py
@@ -32,6 +32,8 @@ specific language governing rights and limitations under the License.
 
 import os
 
+from toml import loads
+
 from .const import DLL_FLD
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -69,25 +71,20 @@ def get_test_fld(abspath=True):
     cwd = os.path.abspath(os.getcwd())
     test_fld = None
 
-    if not os.path.isfile("setup.cfg"):
-        raise FileNotFoundError("setup.cfg configuration file missing in cwd")
+    if not os.path.isfile("pyproject.toml"):
+        raise FileNotFoundError("pyproject.toml configuration file missing in cwd")
 
-    with open("setup.cfg", "r", encoding="utf-8") as f:
-        for line in f:
-            if not "testpaths = " in line:
-                continue
-            test_fld = line.split(" = ", 1)[1].strip()
-            break
+    with open('pyproject.toml', mode = 'r', encoding = 'utf-8') as f:
+        pyproject = loads(f.read())
 
-    if test_fld is None:
-        raise ValueError('setup.cfg does not contain "testpaths" information')
+    test_fld = pyproject['tool']['pytest']['ini_options']['testpaths'][0]
 
     if abspath:
         test_fld = os.path.join(cwd, test_fld)
 
     if not os.path.isdir(test_fld):
         raise ValueError(
-            '"testpaths" in setup.cfg does not point to an existing directory'
+            '"testpaths" in "pyproject.toml" does not point to an existing directory'
         )
 
     return test_fld
