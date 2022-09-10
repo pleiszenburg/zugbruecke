c99a2736287adffba1a751dbd092b15c81de5406
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-09-10 12:12:00 +0200

    year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ master_doc = "index"
 # General information about the project.
 project = "zugbruecke"
 author = "Sebastian M. Ernst"
-copyright = f"2017-2021 {author:s}"
+copyright = f"2017-2022 {author:s}"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
