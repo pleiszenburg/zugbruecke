d98e0799ce77978fe2b368ea241f2557e674dba2
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-12-18 19:32:07 +0100

    memsync prototype

---
+++ src/zugbruecke/core/data/memsync.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+ZUGBRUECKE
+Calling routines in Windows DLLs from Python scripts running on unixlike systems
+https://github.com/pleiszenburg/zugbruecke
+
+    src/zugbruecke/core/data/memsync.py: Memsync definitions
+
+    Required to run on platform / side: [UNIX, WINE]
+
+    Copyright (C) 2017-2022 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/zugbruecke/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from typing import Dict, List
+
+from ..abc import MemsyncABC
+from ..typeguard import typechecked
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typechecked
+class Memsync(MemsyncABC):
+
+    def __init__(self):
+
+        pass
+
+    def as_packed(self) -> Dict:
+
+        return {}
+
+    @classmethod
+    def from_packed(cls, packed) -> MemsyncABC:
+
+        return cls()
+
+    @classmethod
+    def from_dict(cls, definition: Dict) -> MemsyncABC:
+
+        return cls()
+
+    @classmethod
+    def from_dicts(cls, definitions: List[Dict]) -> List[MemsyncABC]:
+
+        return [Memsync.from_dict(definition) for definition in definitions]
