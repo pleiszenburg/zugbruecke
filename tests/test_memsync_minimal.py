ad9ba975cf02444b42bc7771e2e533f94097dfce
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-12-27 02:38:32 +0100

    test cleanup

--- tests/test_memsync_minimal.py
+++ tests/test_memsync_minimal.py
@@ -6,7 +6,7 @@ ZUGBRUECKE
 Calling routines in Windows DLLs from Python scripts running on unixlike systems
 https://github.com/pleiszenburg/zugbruecke
 
-    tests/test_bubblesort.py: Test bidirectional memory sync for pointers
+    tests/test_memsync_minimal.py: Test bidirectional memory sync for pointers
 
     Required to run on platform / side: [UNIX, WINE]
 
@@ -63,38 +63,11 @@ SOURCE = """
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+from typing import List
+
 from .lib.ctypes import get_context
 
 import pytest
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# CLASSES AND ROUTINES
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-class sample_class:
-    def __init__(self, ctypes, dll_handle):
-
-        self._c = ctypes
-        self._bubblesort = dll_handle.bubblesort
-        self._bubblesort.memsync = [  # Regular ctypes on Windows should ignore this statement
-            {
-                "p": [0],  # "path" to argument containing the pointer
-                "l": [1],  # "path" to argument containing the length
-                "t": "c_float",  # type of argument (optional, default char/byte): sizeof(type) * length == bytes
-            }
-        ]
-        self._bubblesort.argtypes = (self._c.POINTER(self._c.c_float), self._c.c_int)
-
-    def bubblesort(self, values):
-
-        ctypes_float_values = ((self._c.c_float) * len(values))(*values)
-        ctypes_float_pointer_firstelement = self._c.cast(
-            self._c.pointer(ctypes_float_values), self._c.POINTER(self._c.c_float)
-        )
-        self._bubblesort(ctypes_float_pointer_firstelement, len(values))
-        values[:] = ctypes_float_values[:]
-
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # TEST(s)
@@ -102,19 +75,33 @@ class sample_class:
 
 
 @pytest.mark.parametrize("arch,conv,ctypes,dll_handle", get_context(__file__))
-def test_bubblesort(arch, conv, ctypes, dll_handle):
+def test_memsync_minimal(arch, conv, ctypes, dll_handle):
 
-    sample = sample_class(ctypes, dll_handle)
+    bubblesort_dll = dll_handle.bubblesort
+    bubblesort_dll.memsync = [  # Regular ctypes on Windows should ignore this statement
+        dict(
+            pointer = [0],  # "path" to argument containing the pointer
+            length = [1],  # "path" to argument containing the length
+            type = ctypes.c_float,  # type of argument (optional, default char/byte): sizeof(type) * length == bytes
+        )
+    ]
+    bubblesort_dll.argtypes = (ctypes.POINTER(ctypes.c_float), ctypes.c_int)
 
-    test_vector = [5.74, 3.72, 6.28, 8.6, 9.34, 6.47, 2.05, 9.09, 4.39, 4.75]
-    sample.bubblesort(test_vector)
-    test_vector = [round(element, 2) for element in test_vector]
-    result_vector = [2.05, 3.72, 4.39, 4.75, 5.74, 6.28, 6.47, 8.6, 9.09, 9.34]
-    vector_diff = sum(
-        [
-            abs(test_vector[index] - result_vector[index])
-            for index in range(len(result_vector))
-        ]
-    )
+    def bubblesort(values: List[int]):
+        """
+        User-facing wrapper around DLL function
+        """
 
-    assert pytest.approx(0.0, 0.0000001) == vector_diff
+        ct_values = ((ctypes.c_float) * len(values))(*values)
+        ct_ptr = ctypes.cast(ctypes.pointer(ct_values), ctypes.POINTER(ctypes.c_float))
+        bubblesort_dll(ct_ptr, len(values))
+        values[:] = ct_values[:]
+
+    data = [5.74, 3.72, 6.28, 8.6, 9.34, 6.47, 2.05, 9.09, 4.39, 4.75]
+    bubblesort(data)
+
+    data = [round(number, 2) for number in data]
+    expected = [2.05, 3.72, 4.39, 4.75, 5.74, 6.28, 6.47, 8.6, 9.09, 9.34]
+    diff = sum(abs(a - b) for a, b in zip(data, expected))
+
+    assert pytest.approx(0.0, 0.0000001) == diff
