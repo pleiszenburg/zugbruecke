b76cb142e7115e74f3d3c1d11c0217f0616b554c
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-12-27 02:51:50 +0100

    test cleanup

--- tests/test_memsync_in_struct.py
+++ tests/test_memsync_in_struct.py
@@ -6,7 +6,7 @@ ZUGBRUECKE
 Calling routines in Windows DLLs from Python scripts running on unixlike systems
 https://github.com/pleiszenburg/zugbruecke
 
-    tests/test_bubblesort_struct.py: Test bidirectional memory sync for pointers in struct
+    tests/test_memsync_in_struct.py: Test bidirectional memory sync for pointers in struct
 
     Required to run on platform / side: [UNIX, WINE]
 
@@ -66,47 +66,11 @@ SOURCE = """
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+from typing import List
+
 from .lib.ctypes import get_context
 
 import pytest
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# CLASSES AND ROUTINES
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-class sample_class:
-    def __init__(self, ctypes, dll_handle):
-
-        self._c = ctypes
-
-        class bubblesort_data(self._c.Structure):
-            _fields_ = [("a", self._c.POINTER(self._c.c_float)), ("n", self._c.c_int)]
-
-        self.bubblesort_data = bubblesort_data
-
-        self._bubblesort_struct = dll_handle.bubblesort_struct
-        self._bubblesort_struct.memsync = [  # Regular ctypes on Windows should ignore this statement
-            {
-                "p": [0, "a"],  # "path" to argument containing the pointer
-                "l": [0, "n"],  # "path" to argument containing the length
-                "t": "c_float",  # type of argument (optional, default char/byte): sizeof(type) * length == bytes
-            }
-        ]
-        self._bubblesort_struct.argtypes = (self._c.POINTER(self.bubblesort_data),)
-
-    def bubblesort_struct(self, values):
-
-        ctypes_float_values = ((self._c.c_float) * len(values))(*values)
-        ctypes_float_pointer_firstelement = self._c.cast(
-            self._c.pointer(ctypes_float_values), self._c.POINTER(self._c.c_float)
-        )
-
-        data = self.bubblesort_data(ctypes_float_pointer_firstelement, len(values))
-
-        self._bubblesort_struct(data)
-        values[:] = ctypes_float_values[:]
-
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # TEST(s)
@@ -114,19 +78,36 @@ class sample_class:
 
 
 @pytest.mark.parametrize("arch,conv,ctypes,dll_handle", get_context(__file__))
-def test_bubblesort_struct(arch, conv, ctypes, dll_handle):
+def test_memsync_in_struct(arch, conv, ctypes, dll_handle):
 
-    sample = sample_class(ctypes, dll_handle)
+    class BubblesortData(ctypes.Structure):
+        _fields_ = [("a", ctypes.POINTER(ctypes.c_float)), ("n", ctypes.c_int)]
 
-    test_vector = [5.74, 3.72, 6.28, 8.6, 9.34, 6.47, 2.05, 9.09, 4.39, 4.75]
-    sample.bubblesort_struct(test_vector)
-    test_vector = [round(element, 2) for element in test_vector]
-    result_vector = [2.05, 3.72, 4.39, 4.75, 5.74, 6.28, 6.47, 8.6, 9.09, 9.34]
-    vector_diff = sum(
-        [
-            abs(test_vector[index] - result_vector[index])
-            for index in range(len(result_vector))
-        ]
-    )
+    bubblesort_struct_dll = dll_handle.bubblesort_struct
+    bubblesort_struct_dll.memsync = [  # Regular ctypes on Windows should ignore this statement
+        dict(
+            pointer = [0, "a"],  # "path" to argument containing the pointer
+            length = [0, "n"],  # "path" to argument containing the length
+            type = ctypes.c_float,  # type of argument (optional, default char/byte): sizeof(type) * length == bytes
+        )
+    ]
+    bubblesort_struct_dll.argtypes = (ctypes.POINTER(BubblesortData),)
 
-    assert pytest.approx(0.0, 0.0000001) == vector_diff
+    def bubblesort_struct(values: List[int]):
+        """
+        User-facing wrapper around DLL function
+        """
+
+        ct_values = ((ctypes.c_float) * len(values))(*values)
+        ct_ptr = ctypes.cast(ctypes.pointer(ct_values), ctypes.POINTER(ctypes.c_float))
+        bubblesort_struct_dll(ctypes.pointer(BubblesortData(ct_ptr, len(values))))
+        values[:] = ct_values[:]
+
+    data = [5.74, 3.72, 6.28, 8.6, 9.34, 6.47, 2.05, 9.09, 4.39, 4.75]
+    bubblesort_struct(data)
+
+    data = [round(number, 2) for number in data]
+    expected = [2.05, 3.72, 4.39, 4.75, 5.74, 6.28, 6.47, 8.6, 9.09, 9.34]
+    diff = sum(abs(a - b) for a, b in zip(data, expected))
+
+    assert pytest.approx(0.0, 0.0000001) == diff
