178514374b4586f1cedf56b9b5119996274fcf15
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-12-27 14:47:33 +0100

    cleaned up test; added multi type support in dll code

--- tests/test_float_types.py
+++ tests/test_float_types.py
@@ -6,7 +6,7 @@ ZUGBRUECKE
 Calling routines in Windows DLLs from Python scripts running on unixlike systems
 https://github.com/pleiszenburg/zugbruecke
 
-    tests/test_in_mantel.py: Tests by value argument passing and return value (float)
+    tests/test_float_types.py: Tests by value argument passing and return value (float)
 
     Required to run on platform / side: [UNIX, WINE]
 
@@ -31,33 +31,44 @@ specific language governing rights and limitations under the License.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 HEADER = """
-{{ PREFIX }} int {{ SUFFIX }} in_mandel(
-	double x0,
-	double y0,
-	int n
-	);
+{% for DTYPE, _, _ in DTYPES %}
+    {{ PREFIX }} int {{ SUFFIX }} in_mandel_{{ DTYPE }}(
+        {{ DTYPE }} x0,
+        {{ DTYPE }} y0,
+        int n
+        );
+{% endfor %}
 """
 
 SOURCE = """
-/* Test if (x0,y0) is in the Mandelbrot set or not */
-{{ PREFIX }} int {{ SUFFIX }} in_mandel(
-	double x0,
-	double y0,
-	int n
-	)
-{
-	double x = 0, y = 0, xtemp;
-	while (n > 0)
-	{
-		xtemp = x * x - y * y + x0;
-		y = 2 * x * y + y0;
-		x = xtemp;
-		n -= 1;
-		if (x * x + y * y > 4) return 0;
-	}
-	return 1;
-}
+{% for DTYPE, _, _ in DTYPES %}
+    {{ PREFIX }} int {{ SUFFIX }} in_mandel_{{ DTYPE }}(
+        {{ DTYPE }} x0,
+        {{ DTYPE }} y0,
+        int n
+        )
+    {
+        /* Test if (x0,y0) is in the Mandelbrot set or not */
+        {{ DTYPE }} x = 0, y = 0, xtemp;
+        while (n > 0)
+        {
+            xtemp = x * x - y * y + x0;
+            y = 2 * x * y + y0;
+            x = xtemp;
+            n -= 1;
+            if (x * x + y * y > 4) return 0;
+        }
+        return 1;
+    }
+{% endfor %}
 """
+
+EXTRA = {
+    "DTYPES": [
+        ("float", "FLT_MIN", "FLT_MAX"),
+        ("double", "DBL_MIN", "DBL_MAX"),
+    ]
+}
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT
@@ -68,36 +79,16 @@ from .lib.ctypes import get_context
 import pytest
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# CLASSES AND ROUTINES
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-class sample_class:
-    def __init__(self, ctypes, dll_handle):
-
-        self.in_mandel = dll_handle.in_mandel
-        self.in_mandel.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_int)
-        self.in_mandel.restype = (
-            ctypes.c_int
-        )  # TODO: sizeof(int) win32 vs win64 vs unix
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # TEST(s)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 @pytest.mark.parametrize("arch,conv,ctypes,dll_handle", get_context(__file__))
-def test_in_mantel_inside(arch, conv, ctypes, dll_handle):
+def test_float_types(arch, conv, ctypes, dll_handle):
 
-    sample = sample_class(ctypes, dll_handle)
+    in_mandel_dll = dll_handle.in_mandel_double
+    in_mandel_dll.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_int)
+    in_mandel_dll.restype = ctypes.c_int
 
-    assert 1 == sample.in_mandel(0.0, 0.0, 500)
-
-
-@pytest.mark.parametrize("arch,conv,ctypes,dll_handle", get_context(__file__))
-def test_in_mantel_outside(arch, conv, ctypes, dll_handle):
-
-    sample = sample_class(ctypes, dll_handle)
-
-    assert 0 == sample.in_mandel(2.0, 1.0, 500)
+    assert 1 == in_mandel_dll(0.0, 0.0, 500)
+    assert 0 == in_mandel_dll(2.0, 1.0, 500)
