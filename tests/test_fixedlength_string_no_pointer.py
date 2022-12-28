5154ec5eb5eb1c85d4b4b4c52f448873f89d3e85
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-12-28 20:21:29 +0100

    test case with setattr on struct object

--- tests/test_fixedlength_string_no_pointer.py
+++ tests/test_fixedlength_string_no_pointer.py
@@ -41,6 +41,13 @@ typedef struct twowords {
     char b[5],
     char (*out)[11]
     );
+
+{% for NAME in COPIES %}
+    {{ PREFIX }} void {{ SUFFIX }} concatenate_fixedlength_in_struct_{{ NAME }}(
+        twowords *somewords,
+        char (*out)[11]
+        );
+{% endfor %}
 """
 
 SOURCE = """
@@ -65,26 +72,32 @@ SOURCE = """
     }
 }
 
-{{ PREFIX }} void {{ SUFFIX }} concatenate_fixedlength_in_struct(
-    twowords *somewords,
-    char (*out)[11]
-    )
-{
-    for (int i = 0; i < 11; i++) {
-        (*out)[i] = ' ';
-    }
-    for (int i = 0; i < 5; i++) {
-        if (somewords->a[i] != '\\0') {
-            (*out)[i] = somewords->a[i];
+{% for NAME in COPIES %}
+    {{ PREFIX }} void {{ SUFFIX }} concatenate_fixedlength_in_struct_{{ NAME }}(
+        twowords *somewords,
+        char (*out)[11]
+        )
+    {
+        for (int i = 0; i < 11; i++) {
+            (*out)[i] = ' ';
+        }
+        for (int i = 0; i < 5; i++) {
+            if (somewords->a[i] != '\\0') {
+                (*out)[i] = somewords->a[i];
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (somewords->b[i] != '\\0') {
+                (*out)[i + 6] = somewords->b[i];
+            }
         }
     }
-    for (int i = 0; i < 5; i++) {
-        if (somewords->b[i] != '\\0') {
-            (*out)[i + 6] = somewords->b[i];
-        }
-    }
-}
+{% endfor %}
 """
+
+EXTRA = {
+    "COPIES": ["f", "g"]
+}
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT
@@ -149,7 +162,43 @@ def test_fixedlength_string_no_pointer_in_struct(arch, conv, ctypes, dll_handle)
             ('b', ctypes.c_char * 5),
         ]
 
-    cconcatenate_fixedlength_in_struct_dll = dll_handle.concatenate_fixedlength_in_struct
+    cconcatenate_fixedlength_in_struct_dll = dll_handle.concatenate_fixedlength_in_struct_f
+    cconcatenate_fixedlength_in_struct_dll.argtypes = (
+        ctypes.POINTER(TwoWords),
+        ctypes.POINTER(ctypes.c_char * 11),
+    )
+
+    def concatenate_fixedlength_in_struct(a: str, b: str) -> str:
+        """
+        User-facing wrapper around DLL function
+        """
+
+        assert len(a) <= 5 and len(b) <= 5
+
+        somewords = ctypes.pointer(TwoWords(a.encode('utf-8'), b.encode('utf-8')))
+        out = ctypes.pointer((ctypes.c_char * 11)())
+
+        cconcatenate_fixedlength_in_struct_dll(somewords, out)
+
+        return out.contents[:].decode('utf-8')
+
+    assert 'Hello world' == concatenate_fixedlength_in_struct('Hello', 'world')
+    assert 'Hell  worl ' == concatenate_fixedlength_in_struct('Hell', 'worl')
+
+
+@pytest.mark.parametrize("arch,conv,ctypes,dll_handle", get_context(__file__))
+def test_fixedlength_string_no_pointer_in_struct_setattr(arch, conv, ctypes, dll_handle):
+    """
+    Test char arrays, fixed length, passed by value within struct via setattr
+    """
+
+    class TwoWords(ctypes.Structure):
+        _fields_ = [
+            ('a', ctypes.c_char * 5),
+            ('b', ctypes.c_char * 5),
+        ]
+
+    cconcatenate_fixedlength_in_struct_dll = dll_handle.concatenate_fixedlength_in_struct_g
     cconcatenate_fixedlength_in_struct_dll.argtypes = (
         ctypes.POINTER(TwoWords),
         ctypes.POINTER(ctypes.c_char * 11),
@@ -164,10 +213,12 @@ def test_fixedlength_string_no_pointer_in_struct(arch, conv, ctypes, dll_handle)
 
         c_char_array5 = ctypes.c_char * 5
 
-        somewords = ctypes.pointer(TwoWords(a.encode('utf-8'), b.encode('utf-8')))
+        somewords = TwoWords()
+        setattr(somewords, 'a', a.encode('utf-8'))
+        setattr(somewords, 'b', b.encode('utf-8'))
         out = ctypes.pointer((ctypes.c_char * 11)())
 
-        cconcatenate_fixedlength_in_struct_dll(somewords, out)
+        cconcatenate_fixedlength_in_struct_dll(ctypes.pointer(somewords), out)
 
         return out.contents[:].decode('utf-8')
 
